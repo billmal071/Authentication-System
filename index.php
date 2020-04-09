aaaa74c2d49dc57aec25a79de120606016c9e636
billmal071 <billmal071@gmail.com>
2020-04-09 22:19:52 +0100

    php task 2

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php  include_once('lib/header.php');  require_once('functions/alert.php'); ?>
+    <p>
+        <?php  print_alert(); ?>
+    </p>
+
+    <div>
+        <h1> Welcome to SNG: Hospital for the ignorant</h1>
+        <p>This is a specialist hospital to cure ignorance!</p>
+        <p>Come as you are, it is completely free!</p>
+        <p>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>            
+        </p>
+    </div>
+
+
+<?php include_once('lib/footer.php'); ?>
+
